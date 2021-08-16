76b79d332491a70fc12d9e39a98a8edaa545d509
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-15 20:53:55 -0700

    Update input_data.py

--- assignments/02AreaTriangulo/tests/input_data.py
+++ assignments/02AreaTriangulo/tests/input_data.py
@@ -7,7 +7,7 @@ input_values = [
         # Inputs
         ["4", "5", "6"],
         # Outputs
-        ["Dame valor a: ", "Dame valor b: ", "Dame valor c: ","Area: 9.921567416492215"],
+        ["Dame valor a:", "Dame valor b:", "Dame valor c:","Area: 9.921567416492215"],
         # Error message
         "Revisa los tipos de dato de tus variables. Revisa la fórmula del área del triángulo."
     ),
@@ -15,7 +15,7 @@ input_values = [
         # Inputs
         ["4", "8", "5"],
         # Outputs
-        ["Dame valor a: ", "Dame valor b: ", "Dame valor c: ","Area:  8.181534085976786"],
+        ["Dame valor a:", "Dame valor b:", "Dame valor c:","Area: 8.181534085976786"],
         # Error message
         "Revisa los tipos de dato de tus variables. Revisa la fórmula del área del triángulo."
 
@@ -25,7 +25,7 @@ input_values = [
         # Inputs
         ["10", "15", "20"],
         # Outputs
-        ["Dame valor a: ", "Dame valor b: ", "Dame valor c: ","Area:  72.61843774138907"],
+        ["Dame valor a:", "Dame valor b:", "Dame valor c:","Area: 72.61843774138907"],
         # Error message
         "Revisa los tipos de dato de tus variables. Revisa la fórmula del área del triángulo."
 
