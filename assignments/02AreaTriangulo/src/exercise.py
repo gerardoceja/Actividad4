58fa5eb21c60b08ac8f3fcd59baf523ee58be7fa
gerardoceja <89027066+gerardoceja@users.noreply.github.com>
2021-08-18 19:19:21 -0700

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,7 +1,18 @@
 import math
 def main():
      #escribe tu código abajo de esta línea
-   pass
+   import math
+
+a = float(input("Ingrese la longitud del lado A: "))
+b = float(input("Ingrese la longitud del lado B: "))
+c = float(input("Ingrese la longitud del lado C: "))
+
+s = (a + b + c) / 2
+
+ar= math.sqrt(s*(s-a)*(s-b)*(s-c))
+
+print("Area del triangulo: ",ar)
+
 
 
 if __name__ == '__main__':
