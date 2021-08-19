721aa84bccab7afa5b9e760630e5fc37c78246df
gerardoceja <89027066+gerardoceja@users.noreply.github.com>
2021-08-18 19:14:16 -0700

    Update exercise.py

--- assignments/01AreayVolumen/src/exercise.py
+++ assignments/01AreayVolumen/src/exercise.py
@@ -1,7 +1,15 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    import math
+
+r = float(input("Ingrese el radio de la esfera: "))
+
+a = 4 * math.pi * r**2
+v= (4 * math.pi * r**3) / 3
+
+print("Area de la esfera: ",a)
+print("Volumen de la esfera: ",v)
 
 
 
